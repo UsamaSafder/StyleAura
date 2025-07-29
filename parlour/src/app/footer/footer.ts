499fad62cc606a70bd3dc230e45748d6bbaf0e69
Usama <usama18053lf@gmail.com>
2025-07-30 00:05:38 +0500

    some changes to footer font andn styling made and router links were added

--- parlour/src/app/footer/footer.ts
+++ parlour/src/app/footer/footer.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
 export class Footer {
-
+scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 }
