da9fccf1482671e76e171df6e085ea0de9288727
Usama <usama18053lf@gmail.com>
2025-07-30 16:44:53 +0500

    Services page ready with Icons added in header footer and home page and then in services page

--- parlour/src/app/services/services.ts
+++ parlour/src/app/services/services.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Footer } from '../footer/footer';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [RouterOutlet,Footer,NgFor,RouterLink],
   templateUrl: './services.html',
   styleUrl: './services.css'
 })
 export class Services {
-
+   services = [
+    { title: 'Party Makeup', image: 'party.jpg' },
+    { title: 'Bridal Makeup', image: 'bridal.jpg' },
+    { title: 'Manicure Pedicure', image: 'bridal.jpg' },
+    { title: 'Hair Cut', image: 'hairstyle.jpg' },
+    { title: 'Mehndi Art', image: 'mehndi.jpg' },
+    { title: 'Hydra Facial', image: 'bridal.jpg' },
+    { title: 'Hair Coloring', image: 'bridal.jpg' },
+    { title: 'Threading', image: 'bridal.jpg' },
+    { title: 'Waxing', image: 'bridal.jpg' },
+    { title: 'Nail Art', image: 'nail.jpg' },
+    { title: 'Hair Styling', image: 'hairstyle.jpg' },
+    { title: 'Body Massage', image: 'mehndi.jpg' },
+    { title: 'Skin Polish', image: 'bridal.jpg' },
+    { title: 'Makeover', image: 'hairstyle.jpg' },
+    { title: 'Face Bleach', image: 'bridal.jpg' },
+  ];
 }
