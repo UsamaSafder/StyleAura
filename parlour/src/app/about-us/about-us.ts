9e546ee344d215a6441e262bc1c7bf619ad36121
Usama <usama18053lf@gmail.com>
2025-07-30 17:38:20 +0500

    About Page is Ready with full Css

--- parlour/src/app/about-us/about-us.ts
+++ parlour/src/app/about-us/about-us.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Footer } from '../footer/footer';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [Footer,CommonModule,RouterOutlet,RouterLink],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css'
 })
